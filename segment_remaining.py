1a56d7fb6db41e8267910b8d02aa55006f6865c3
Michael Rariden <mr2ep@virginia.edu>
2025-02-07 23:03:35 -0500

    allow laminin only or laminin+actin with flag

--- segment_remaining.py
+++ segment_remaining.py
@@ -8,7 +8,7 @@ import os
 
 rv_cp_dir = "/scratch/" + os.environ.get('USER') + "/rivanna_cellpose"
 
-def segment_images(base_dir, slurm_script):
+def segment_images(base_dir, slurm_script, use_laminin_only):
     """
     Script to segment a folder of images by finding the remaining masks not in the mask dir.
     It then submits a job to SLURM to process each image.
@@ -18,10 +18,19 @@ def segment_images(base_dir, slurm_script):
     slurm_script (str): Path to the SLURM script used for submitting the job.
     mask_output_dir (str): Directory where the output masks should be saved.
     """
+    
     base_path = Path(base_dir)
 
     original_imgs_path = base_path / 'original_images'
-    masks_path = base_path / 'fiber_masks'
+    if use_laminin_only:
+        masks_path = base_path / 'fiber_masks_laminin-only'
+        
+        print(f"Using laminin only and saving to {str(masks_path.resolve())}")
+    else:
+        masks_path = base_path / 'fiber_masks'
+        
+        print(f"Using laminin+actin and saving to {str(masks_path.resolve())}")
+        
     
     # Remove the /sfs/weka/ where the /scratch dir might be mounted: 
     masks_str = str(masks_path.resolve())
@@ -48,11 +57,17 @@ def segment_images(base_dir, slurm_script):
         
         # Command order: script, full image path, mask out dir
         cmd = ['sbatch', slurm_script, img_str, masks_str]
-        subprocess.run(cmd)
+        # subprocess.run(cmd)
+        
+        print(cmd)
+        break
+        
 
 def main():
     parser = argparse.ArgumentParser(description="Segment images using cellpose via SLURM.")
     parser.add_argument("base_dir", type=str, help="Base directory containing image folders.")
+    parser.add_argument("-L", "--laminin_only", default=False, action='store_true',
+                        help='Use laminin model instead of LA model to segment myofibers')
     parser.add_argument("-s", "--slurm_script", type=str, 
             default=rv_cp_dir + "/fiber_seg_CLI_OME-model_rvcp-modules.slurm", 
             help="Path to the SLURM script for image segmentation."
@@ -60,8 +75,13 @@ def main():
             )
 
     args = parser.parse_args()
+    
+    if args.laminin_only:
+        slurm_script = rv_cp_dir + "/fiber_seg_CLI_OME-model_rvcp-modules_laminin.slurm"
+    else:
+        slurm_script = args.slurm_script
 
-    segment_images(args.base_dir, args.slurm_script)
+    segment_images(args.base_dir, slurm_script, args.laminin_only)
 
 if __name__ == "__main__":
     main()
